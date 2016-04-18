f090c686e534318d960c782474b1e2a60aa0bc5e
Mathias Stig Jensen <erase@hotmail.dk>
2016-04-18 17:54:19 +0200

    Start on basic world
    Basic layout for the game, + the dice random generator.

---
+++ Assets/Scripts/DiceGenerator.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DiceGenerator : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+
+    public void startGenerator()
+    {
+        randomGenerator();
+    }
+
+
+    int randomGenerator()
+    {
+        float rng =  Random.Range(2, 18);
+        int tmp = Mathf.CeilToInt(rng / 3);
+        print(tmp);
+        return tmp;
+    }
+
+
+
+}
